c6c9997aa3688d7be174480adf660ed499bf05ab
Diptajyoti Mukherjee <mukherjeed2@allegheny.edu>
2017-06-14 16:34:55 +0200

    Changes in plotting script

--- week2/plottingScript.py
+++ week2/plottingScript.py
@@ -4,19 +4,26 @@ import math
 import numpy
 import sys
 
-mu = constants.G.value_in((units.km)**3 /(units.MSun * units.s **2))
+mu = constants.G.value_in((units.km)**3 /(units.s **2))
 
+fileName = sys.argv[1]
+f = h5py.File(fileName,'r')
+vx = []
+vy = []
+vz = []
 
-
+x = []
+y = []
+z = []
 
 
 def orbital_elements_test_particles(xx,yy,zz,vxx,vyy,vzz):
     #Using vis-viva equation
     m = 1 
-    dist = math.sqrt(xx ** 2 + yy ** 2 + zz ** 2)
+    dist = (math.sqrt(xx ** 2 + yy ** 2 + zz ** 2)) | units.AU
     v_squared = vxx ** 2 + vyy ** 2 + vzz ** 2
     
-    one_over_a = 2/dist - ((v_squared/mu) | (1/units.km)).value_in(1/units.AU)
+    one_over_a = 2/dist.value_in(units.AU) - ((v_squared/mu) | (1/units.km)).value_in(1/units.AU)
     a = 1/one_over_a
 
     x_vec = numpy.array([xx,yy,zz])
@@ -27,25 +34,38 @@ def orbital_elements_test_particles(xx,yy,zz,vxx,vyy,vzz):
 
     inc = numpy.arccos(numpy.dot(h_vec, z_vec) / (numpy.linalg.norm(h_vec) * numpy.linalg.norm(z_vec))) * 360 / (2 * math.pi)
 
-    epsilon = (v_squared / 2) - (mu / dist) # change units
+    epsilon = ((v_squared / 2) - (mu / dist.value_in(km))) | (units.km ** 2 / units.s **2)
 
-    ecc = math.sqrt(1+2*epsilon*numpy.dot(h_vec,h_vec)/(mu ** 2))
-
+    ecc = math.sqrt(1+2*epsilon.value_in(units.km **2 / units.s **2)*(numpy.dot(h_vec,h_vec)*((1 | units.AU ** 2).value_in(units.km **2)))/(mu ** 2)) #let's hope this works!
     return a , ecc, inc 
 
-f = h5py.File('solartest.hdf5','r')
+def get_attributes():
+    attributes = f['particles']['0000000100']['attributes']
+    vx = attributes['vx']
+    vy = attributes['vy']
+    vz = attributes['vz']
 
-attributes = f['particles']['0000000100']['attributes']
-vx = attributes['vx']
-vy = attributes['vy']
-vz = attributes['vz']
+    x = attributes['x']
+    y = attributes['y']
+    z = attributes['z']
 
-x = attributes['x']
-y = attributes['y']
-z = attributes['z']
 
-for i in range(0,9):
-	a , ecc , inc = orbital_elements_test_particles(x[i],y[i],z[i],vx[i],vy[i],vz[i])
-	print a , inc 
-f.close()
+def plot():
+    pyplot.xlim(0,100000)
+    pyplot.xscale('log')
+    pyplot.ylim(0,360)
+    numOfParticles = len(vx)
+    for i in range(0,numOfParticles):
+        if (x[i] != 0 and y[i] != 0 and z[i]!= 0):
+            semi_major , ecc , inc = orbital_elements_test_particles(x[i],y[i],z[i],vx[i],vy[i],vz[i])
+            pyplot.plot(semi_major,i,'ro')
+        else:
+            continue
+    pyplot.savefig()#enternameofFile
+
+    f.close()
+
+if __name__ == '__main__':
+    get_attributes()
+    plot()
 
